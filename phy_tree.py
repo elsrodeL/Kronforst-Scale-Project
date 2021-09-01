15d9791730f65e9a098851370fb0c4e3c81a721f
elsrodeL <58862171+elsrodeL@users.noreply.github.com>
2021-09-01 18:34:10 -0500

    Update phy_tree.py

--- phy_tree.py
+++ phy_tree.py
@@ -1,25 +1,30 @@
-# Lukas Elsrode - phy_tree.py (08/27/2020)
+"""phy_tree.py
+Class to Visulize the Phylogenetic Relationships/Connections in the Data 
 
-'''Vizulizes the phylogony of the data used
+    SCALE PROJECT -- KRONFORST LABORATORY AT THE UNIVERSITY OF CHICAGO 
+                  -- ALL RIGHTS RESERVED 
+        
+        Lukas Elsrode - Undergraduate Researcher at the Kronforst Laboratory wrote and tested this code 
+        (09/01/2021)
 
-'''
+"""
 
-import math
-import scale_data
 import networkx as nx
+import scale_data as data 
 import matplotlib.pyplot as plt
 
 
 class Trunk:
-    def __init__ (self):
+    def __init__(self):
         self.graph = self.mk_tree_trunk()
         self.all_fams = [
             'lycaenidae', 'nymphalidae',
             'zyganidae', 'saturniidae',
             'papilionidae', 'hesperiidae',
             'uraniidae', 'pieridae']
-        self.traversal_nodes = [i for i in list(self.graph.nodes) if i not in self.all_fams]
-    
+        self.traversal_nodes = [i for i in list(
+            self.graph.nodes) if i not in self.all_fams]
+
     def mk_tree_trunk(self):
         G = nx.Graph()
 
@@ -60,81 +65,82 @@ class Trunk:
 global stem
 stem = Trunk()
 
+
 class Tree:
     def __init__(self, df, trunk=stem):
-
         # Constructor Data
         self.trunk = trunk.graph                # Manual Graph
         self.trunk_fams = trunk.all_fams        # Families in DB
         self.t_nodes = trunk.traversal_nodes    # Traversal Nodes
-
         # Construction Params
         self.df = df                            # Scale Data
-
         # init
         self.tree = None
         self.complete()
 
     def complete(self):
         self.fill_tree()
-        print('Do you wish you simplify Phylogentic Tree to only non-reducable nodes ? [Y/N]')
-        reduce_ = input()
-        if reduce_ == 'Y':
-            self.simplify_once()
+        self.simplify_once()
         show_tree(self)
         return
 
     def fill_tree(self):
-
         # init
         self.tree = self.trunk
-
-        # Itterate
-        hierarchy = list(self.df.columns[1:5])
-        count = 0
+        # Get the depth of the Hierarchy you want to Itterate over
+        hierarchy, count = list(self.df.columns[1:5]),0
         while count < (len(hierarchy) - 1):
+            # Get the corresponding Data to the Current lvl in the tree
             lvl = hierarchy[count]
             nodes = [i for i in set(self.df[lvl].values)]
+            # 
             for p_node in nodes:
                 node_df = self.df.loc[self.df[lvl] == p_node]
                 sub_lvl = hierarchy[count + 1]
                 children = [i for i in set(node_df[sub_lvl].values)]
 
-                ## ADD SUBFAMILY NODE AND CONNECT TO EXISTING TRUNK FAMILY NODE BY EDGE
+                # ADD SUBFAMILY NODE AND CONNECT TO EXISTING TRUNK FAMILY NODE BY EDGE
                 if p_node != '' and count == 0:
                     self.tree.add_node(p_node)  # ++ NODE
-                    fams = list(set(self.df.loc[self.df[lvl] == p_node]['family'].values))
+                    fams = list(
+                        set(self.df.loc[self.df[lvl] == p_node]['family'].values))
                     if len(fams) == 1 and fams[0] != '':
-                        self.tree.add_edge(fams[0], p_node) # ++ EDGE
+                        self.tree.add_edge(fams[0], p_node)  # ++ EDGE
 
                 grand_lvl = hierarchy[count - 1]
                 for child in children:
 
-                    ## ADD NODE TO GRANDPARENT IF PARENT IS VOID
+                    # ADD NODE TO GRANDPARENT IF PARENT IS VOID
                     if p_node == '' and child != '':
-                        grandparent = list(self.df.loc[self.df[sub_lvl] == child][grand_lvl].values)[0]
+                        grandparent = list(
+                            self.df.loc[self.df[sub_lvl] == child][grand_lvl].values)[0]
                         if grandparent != '':
-                            self.tree.add_edge(child, grandparent)  ## ++ CHILD <-> GPARENT EDGE
+                            # ++ CHILD <-> GPARENT EDGE
+                            self.tree.add_edge(child, grandparent)
                         else:
                             pass
                     if child == '':
                         dsub_lvl = hierarchy[count + 2]
-                        gchildren = list(self.df.loc[self.df[lvl] == p_node][dsub_lvl].values)
-                        comp = list(self.df.loc[self.df[sub_lvl] == child][dsub_lvl].values)
+                        gchildren = list(
+                            self.df.loc[self.df[lvl] == p_node][dsub_lvl].values)
+                        comp = list(
+                            self.df.loc[self.df[sub_lvl] == child][dsub_lvl].values)
                         gr_children = [i for i in gchildren if i in comp]
 
-                        ## IF THE CHILD IS VOID BUT PARENT IS NOT CONNECT PARENT TO GRANDCHILD
+                        # IF THE CHILD IS VOID BUT PARENT IS NOT CONNECT PARENT TO GRANDCHILD
                         for granchild in gr_children:
                             if granchild != '' and p_node != '':
-                                self.tree.add_edge(granchild, p_node) ## ++ G_CHILD <-> PARENT  EDGE
+                                # ++ G_CHILD <-> PARENT  EDGE
+                                self.tree.add_edge(granchild, p_node)
                             else:
                                 pass
 
                     else:
-                        ## ADD CHILD TO PARENT NODE 
-                        self.tree.add_node(child)       ## ++ CHILD NODE
+                        # ADD CHILD TO PARENT NODE
+                        self.tree.add_node(child)  # ++ CHILD NODE
                         if p_node != '':
-                            self.tree.add_edge(p_node, child)   ## ++ PARENT <-> CHILD EDGE
+                            # ++ PARENT <-> CHILD EDGE
+                            self.tree.add_edge(p_node, child)
                         else:
                             pass
             count += 1
@@ -142,11 +148,11 @@ class Tree:
         # Remove Manually Constructed Family Nodes which don't appear in our data
         all_famis = [i for i in set(self.df.family.values) if i != '']
         to_remove = [i for i in self.trunk_fams if i not in all_famis]
-        
+
         for node in to_remove:
             if node in list(self.tree.nodes):
                 self.tree.remove_node(node)
-        
+
         return self.tree
 
     def simplify_once(self):
@@ -166,7 +172,7 @@ class Tree:
 
         for l in leaves:
             # Create a Deapth first search list from a leaf
-            search_list = list(nx.dfs_preorder_nodes(g,l))
+            search_list = list(nx.dfs_preorder_nodes(g, l))
 
             # Pointer to list to itterate
             j = 0
@@ -193,32 +199,31 @@ class Tree:
         for r in reducables:
             g.remove_node(r)
 
-        for k,v in d.items():
-            g.add_edge(k,v)
+        for k, v in d.items():
+            g.add_edge(k, v)
 
         for l in leaves:
             if l in self.t_nodes:
                 if l != 'r':
                     g.remove_node(l)
 
-        
         self.tree = g
 
         return self.tree
-
 
 
 def pos_nodes(Tree):
     # Vizual Layout
     return nx.kamada_kawai_layout(Tree.tree, dim=2)
 
-def set_node_atts(Tree,p,nodes,color='red',size=700)
-    nx.draw_networkx_nodes(Tree.tree,p,nodelist=nodes,node_color=color,node_size=size)
-    
+
+def set_node_atts(Tree, p, nodes, color='red', size=700):
+    nx.draw_networkx_nodes(Tree.tree, p, nodelist=nodes,node_color=color, node_size=size)
+
+
 def draw_tree(Tree):
     # Draw the Kamada Kawaii
     nx.draw_kamada_kawai(self.tree, with_labels=1)
     plt.axis('off')
     plt.show()
     return
-
