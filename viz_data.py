cf4ac5a5e2f8d53ad4d1337c2abc0af468ca0666
Lukas Elsrode <58862171+elsrodeL@users.noreply.github.com>
2021-10-11 12:37:58 -0500

    Vizulizations basically done

--- viz_data.py
+++ viz_data.py
@@ -1,22 +1,24 @@
-"""viz_data.py
+""" viz_data.py
 
 Functions to Visualize the Data
 
-    SCALE PROJECT -- KRONFORST LABORATORY AT THE UNIVERSITY OF CHICAGO 
-                  -- ALL RIGHTS RESERVED 
-        
-        Lukas Elsrode - Undergraduate Researcher at the Kronforst Laboratory wrote and tested this code 
-        (09/01/2021)
-        
+    SCALE PROJECT -- KRONFORST LABORATORY AT THE UNIVERSITY OF CHICAGO
+                  -- ALL RIGHTS RESERVED
+
+        Lukas Elsrode - Undergraduate Researcher at the Kronforst Laboratory wrote and tested this code
+        (10/11/2021)
+
 """
+import itertools
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 from sklearn.decomposition import PCA
 from scale_data import segment_df_by_field
+from copy import deepcopy
 
-
+# These are the default morphometric features of our ultra-structures of diffrent scales
 DEF_FEATURES = [
     'lacuna_area_window',
     'lacuna_perimeter',
@@ -33,7 +35,7 @@ def make_title(df):
             e.g if data comprised of all Nymphalids that are Black >> nymphalidia black
 
                 Inputs:
-                    ('Pandas.DataFrame' Object Class) - 'df' : The DataSet used 
+                    ('Pandas.DataFrame' Object Class) - 'df' : The DataSet used
                 Outputs:
                     (string)  - The unifying common family & color which unites our dataset
     """
@@ -66,7 +68,7 @@ def feature_distribution(df, feature='scale_color', segby='f'):
 
         Inputs:
             ('Pandas.DataFrame' Object Class) - 'df' : DataFrame to plot our Morphometric measurements
-            (string) - 'feature' : What The Classes should be split up by 
+            (string) - 'feature' : What The Classes should be split up by
             (string) - 'segby': By What Class attribute in the Column of the dataset should the data be split up by
 
         Outputs:
@@ -82,12 +84,10 @@ def feature_distribution(df, feature='scale_color', segby='f'):
             plt.show()
     return
 
-# TO TEST
-
 
 def run_viz_by_field(df, foo, c_map, field, segby='f'):
-    """Recursively runs through visualization function 'foo' to our data segmented 'df' 
-    by a group 'f', running each visualization observing variable 'field' and applies a 
+    """Recursively runs through visualization function 'foo' to our data segmented 'df'
+    by a group 'f', running each visualization observing variable 'field' and applies a
     color mapping 'c_map' to those visualization.
 
         Inputs:
@@ -95,10 +95,10 @@ def run_viz_by_field(df, foo, c_map, field, segby='f'):
             ('Function' Python Object Class) - 'foo' : function in viz_data.py that takes a 'color mapping' and 'field' as inputs
             (Dictionary) - 'c_map': Dictionary Mapping Field values to the color descriptions for the Visulizations
             (string) - 'field': What Variable to classify the plot against i.e 'scale_color'
-            (string) - what to segment the data by i.e 'f'- by family 
+            (string) - what to segment the data by i.e 'f'- by family
 
         Outputs:
-            (None) - Plots Visualizations 
+            (None) - Plots Visualizations
 
     """
 
@@ -112,8 +112,6 @@ def run_viz_by_field(df, foo, c_map, field, segby='f'):
             foo(d, c_map=c_map, field=field)
     return
 
-# TO TEST
-
 
 def show_originaldim(df, c_map, features=DEF_FEATURES, field='scale_color'):
     """Shows original feature distribution of our dataset
@@ -123,7 +121,7 @@ def show_originaldim(df, c_map, features=DEF_FEATURES, field='scale_color'):
             ('Pandas.DataFrame' Object Class) - 'df' : DataFrame to plot our Morphometric measurements
             (Dictionary) - 'c_map': Dictionary Mapping Field values to the color descriptions for the Visulizations
             (List of Strings) - 'features': the column names for the morphometric measurements to be examined
-            (String) - 'field': By what column is the data divided by 
+            (String) - 'field': By what column is the data divided by
         Outputs:
             (None)
     """
@@ -138,35 +136,26 @@ def show_originaldim(df, c_map, features=DEF_FEATURES, field='scale_color'):
 
     fig.update_traces(diagonal_visible=False)
     fig.show()
-    plt.savefig('test.png')
-    return
-
-
-def PCA_2D(df, c_map, features=DEF_FEATURES, field='scale_color'):
-    '''Makes a Two component PCA of data given
-    '''
-    df_n, X = resize_data(df, features, field)
-    if type(df_n) != int:
-        pca = PCA(n_components=2)
-        components = pca.fit_transform(X)
-        fig = px.scatter(
-            components, x=0, y=1, color=df_n[field],
-            title='2-Component PCA',
-            labels={'0': 'PC1', '1': 'PC2'},
-            color_discrete_map=c_map)
-        fig.show()
     return
 
 
 def PCA_3D(df, c_map, features=DEF_FEATURES, field='scale_color'):
-    '''Makes a Three component PCA of data given
-    '''
+    """ Makes a 3-component PCA of data given, Dimensional Reduction to 3 dimensions
+
+        Inputs:
+            ('Pandas.DataFrame' Object Class) - 'df': The DataFrame to plot our morphometric measurements
+            (Dictionary) - 'c_map': Dictionary Mapping Field values to the color descriptions for the Visualizations
+            (List of Strings) - 'features': the column names for the morphometric measurements to be examined
+            (String) - 'field': What is the target variable in the dataset
+        Outputs:
+            (None)
+    """
     df_n, X = resize_data(df, features, field)
     if type(df_n) != int:
 
         pca = PCA(n_components=3)
         components = pca.fit_transform(X)
-
+        print_axis_components(pca, features)
         fig = px.scatter_3d(
             components, x=0, y=1, z=2, color=df_n[field],
             title='3-Component PCA',
@@ -177,35 +166,34 @@ def PCA_3D(df, c_map, features=DEF_FEATURES, field='scale_color'):
         return
 
 
-def mk_subset_of_PC_components(df, c_map, N=4, features=DEF_FEATURES, field='scale_color'):
+def print_axis_components(pca, features):
+    """ For a PCA model list the number of components and show the normalized wheights of each of the features.
 
-    df_n, X = resize_data(df, features, field)
+        Inputs:
+            ('sklearn.decomposition._pca.PCA' Object Class) - 'pca' : The principle component axes
+            (List of Strings) - 'features': The column names of the features making up the axes
+        Outputs:
+            (None)
 
-    if type(df_n) != int:
-        # make a high dimentional reduction to explain as much variance as possible
-        pca = PCA(n_components=N)
-        components = pca.fit_transform(X)
-
-        total_var = pca.explained_variance_ratio_.sum() * 100
-
-        labels = {str(i): f"PC {i+1}" for i in range(N)}
-        labels['color'] = field
-
-        fig = px.scatter_matrix(
-            components,
-            color=df_n[field],
-            dimensions=range(N),
-            labels=labels,
-            title=f'Total Explained Variance: {total_var:.2f}%',
-            color_discrete_map=c_map
-        )
-        fig.update_traces(diagonal_visible=False)
-        fig.show()
-    return
+    """
+    for i, paxis in enumerate(pca.components_):
+        print(f"~ PC{str(i+1)} ~")
+        for j, comp in enumerate(paxis):
+            print(f"{features[j]} : {comp}")
+        print('\n')
 
 
 def mk_explained_variance_curve(df, features=DEF_FEATURES, field='scale_color'):
+    """ Given The Data Set and features how much vairance is explaied per number of principle of component
+        axes added
 
+        Inputs:
+            ('Pandas.DataFrame' Object Class) - 'df': The DataFrame to plot our morphometric measurements
+            (List of Strings) - 'features': the column names for the morphometric measurements to be examined
+            (String) - 'field': What is the target variable in the dataset
+        Outputs:
+            (None)
+    """
     df_n, X = resize_data(df, features, field)
     if type(df_n) != int:
 
@@ -216,20 +204,29 @@ def mk_explained_variance_curve(df, features=DEF_FEATURES, field='scale_color'):
         fig = px.area(
             x=range(1, exp_var_cumul.shape[0] + 1),
             y=exp_var_cumul,
-            labels={"x": "# Components", "y": "Explained Variance"}
+            labels={"x": "# Components", "y": "Explained Variance"},
+            title=make_title(df)
         )
-        print(make_title(df))
         fig.show()
     return
 
 
 def Load_Features(df, c_map, features=DEF_FEATURES, field='scale_color'):
+    """ Creates a 2D PCA and Shows you the vector components of features to each axis.
 
+        Inputs:
+            ('Pandas.DataFrame' Object Class) - 'df': The DataFrame to plot our morphometric measurements
+            (Dictionary) - 'c_map': Dictionary Mapping Field values to the color descriptions for the Visulizations
+            (List of Strings) - 'features': the column names for the morphometric measurements to be examined
+            (String) - 'field': What is the target variable in the dataset
+        Outputs:
+            (None)
+    """
     df_n, X = resize_data(df, features, field)
-
     if type(df_n) != int:
         pca = PCA(n_components=2)
         components = pca.fit_transform(X)
+        print_axis_components(pca, features)
         loadings = pca.components_.T * np.sqrt(pca.explained_variance_)
 
         fig = px.scatter(components, x=0, y=1, title=make_title(df), labels={
@@ -255,6 +252,15 @@ def Load_Features(df, c_map, features=DEF_FEATURES, field='scale_color'):
 
 
 def resize_data(df, features, field):
+    """ Given DataSet with a field to be determined from Features drop any rows which have missing feature inputs
+
+        Inputs:
+            ('Pandas.DataFrame' Object Class) - 'df': The DataFrame to plot our morphometric measurements
+            (List of Strings) - 'features': the column names for the morphometric measurements to be examined
+            (String) - 'field': What is the target variable in the dataset
+        Outputs:
+            (tuple) - 'df_n', 'X' : The new resized data, the indicator variables columns from the resized data
+    """
     r = features + [field]
     df_n = df[r]
     df_n = df_n.dropna()
@@ -267,3 +273,74 @@ def resize_data(df, features, field):
         return 0
     else:
         return df_n, X
+
+
+# GET ALL POSSIBLE SUBSETS OF FEATURES
+def get_all_possible_combinations(features=DEF_FEATURES):
+    """ Get a list of all the possible subsets of features from containing none [] to all features [x0,x1,...,xn]
+
+        Inputs:
+            (List) - 'features': A list of the morphological features which could be ploted in one plane (i.e 'DEF_FEATURES')
+        Outputs:
+            (List) - 'features_all_combinations': A list of all the possible subsets of the original features list (i.e [[],[x0],[x1,x0],[x0,x2],...,[x0,x1,...,xn])
+    """
+    features_all_combinations = []
+    for N in range(0, len(features)+1):
+        for feat_subset in itertools.combinations(features, N):
+            features_all_combinations.append(list(feat_subset))
+    return features_all_combinations
+
+
+# GET THE BEST POSSIBLE FEATURE SET AND DISPLAY IT
+def optimize_feature_set(df, c_map, min_num_features=2, field='scale_color', components=2):
+    """ Return The best Set of  Features given the input feature sets and the desired data
+
+        Inputs:
+            ('Pandas.DataFrame' Object Class) - 'df': The DataFrame to plot our morphometric measurements
+            (Dictionary) - 'c_map': Dictionary Mapping Field values to the color descriptions for the Visulizations
+            (Int) - 'min_num_features' : The minimum number of Features the PCA axes can be constructed from
+            (components) - 'components': The number of components the PCA can have.
+        Outputs:
+            (List of Strings) - Best Feature Sub-Set given selection methedology
+    """
+    assert components in set(
+        [2, 3]), 'Reductions above 3 dimensions and below 2 dimensions are not possible '
+    # Change Up the conditions so our Data is still Meaningful
+    feature_sets = get_all_possible_combinations()
+    feature_sets = [x for x in feature_sets if len(x) >= min_num_features]
+    # Get a way to store ~ (feature_list,pca_explained_vairance_ratio)
+    rv = [None]*len(feature_sets)
+    # Fill rv[i] with values of explained variance score of feature_sets[i]
+    for i, fset in enumerate(feature_sets):
+        try:
+            # Here we generate the 'Samples Data', 'Wilde Type Data', and  'Mutant Data'
+            data = deepcopy(df)
+            df_n, X = resize_data(data, fset, field)
+            if type(df_n) != int:
+                pca = PCA(n_components=components)
+                pca.fit_transform(X)
+                score = round(sum(list(pca.explained_variance_ratio_))*100, 2)
+                entry = fset, score
+                rv[i] = entry
+        except:
+            entry = fset, 0.0
+            rv[i] = entry
+            continue
+
+    # get the key in the (k,v) pair tuples filling rv with lambda
+    best_entry = max(rv, key=lambda i: i[1])
+    best_features, _ = best_entry
+
+    # Run appropriate visualizations
+    if components == 2:
+        # Show The Loaded PCA
+        Load_Features(df, c_map, best_features, field)
+
+    else:
+        # Show a 3D model PCA
+        PCA_3D(df, c_map, best_features, field)
+
+    print('\n')
+    # Show How Explained Variance Grows with added axes
+    mk_explained_variance_curve(df, best_features, field)
+    return best_features
