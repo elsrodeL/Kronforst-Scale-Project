1569cd6b6df7c97d89b766f68e4569d2ab156412
Lukas Elsrode <58862171+elsrodeL@users.noreply.github.com>
2021-10-15 17:41:08 -0500

    new main

--- main.py
+++ main.py
@@ -1,53 +1,74 @@
-#  Lukas Elsrode (2021)
+""" main.py
 
-import scale_data
-import viz_data
+    Runs Our Program.   
+
+"""
+
+# The Source Code for this Project can be found in these libraries
+import sys
+import pyfiglet
 import color
-from phy_tree import Tree
-from time import sleep
+import viz_data
+from scale_data import WT_DATA, SAMPLES_DATA, MUTANT_DATA
+from copy import deepcopy
 
 
-def gen_rgb_data():
-    # Switch the 'scale_color' field in both datasets to the RGB name
-    info, data = color.use_RBG(
-        scale_data.df_samples, scale_data.df_data, colors=None)
-    # Add the 'R','G','B' columns
-    data = color.make_R_G_B_cols(data)
-    return info, data
+def main(dataset='closest', analysis='family', colors=None):
+    """ ~ Main Function For our Program ~
 
+        Inputs:
+            dataset
+            ______________
+                - 'closest'    - Gives you closest color in colors to the labeled scale color.
+                - 'validated'  - Gives you data who's color description is validated by a sampled RGB value.
+                - 'rgb'        - Gives you closest Definable scale color as defined by the CSS3 library.
 
-def gen_validated_by_data():
-    info, data = color.drop_misclassified_colors(
-        scale_data.df_samples, scale_data.df_data)
-    data = color.make_R_G_B_cols(data)
-    return info, data
+            analysis:
+            ____________
+                - 'family'    - Does a Family by Family Analysis of the Scale colors.
+                - 'mutant'    - Does a Mutant /CRISPR CAS9 analysis for pre and post mutation ultra-structures.
+    """
+    # Init our main Function
+    WT, SAMPLES, MUTANTS = deepcopy(WT_DATA), deepcopy(
+        SAMPLES_DATA), deepcopy(MUTANT_DATA)
+    assert analysis in set(['mutant', 'family']
+                           ), "Choose Analysis Type : 'mutant' or 'family' "
 
+    # Case of family analysis
+    if analysis == 'family' and dataset == 'rgb':
+        samples, data = color.gen_rgb_data(SAMPLES_DATA, WT_DATA)
 
-def gen_custom_closest(colors):
-    info, data = color.use_RBG(
-        scale_data.df_samples, scale_data.df_data, colors=colors)
-    # Add the 'R','G','B' columns
-    data = color.make_R_G_B_cols(data)
-    return info, data
+    if analysis == 'family' and dataset == 'validated':
+        samples, data = color.gen_validated_by_data(SAMPLES_DATA, WT_DATA)
+
+    if analysis == 'family' and dataset == 'closest':
+        if type(colors) != list:
+            samples, data = color.gen_custom_closest(SAMPLES_DATA, WT_DATA)
+        else:
+            samples, data = color.gen_custom_closest(
+                SAMPLES_DATA, WT_DATA, colors)
+
+        viz_data.wt_analysis(data)
+
+    # Case of Mutants
+    else:
+        # Assign Correct Mutant DataFrame
+        if dataset == 'rgb':
+            samples, data = color.gen_rgb_data(
+                SAMPLES_DATA, MUTANT_DATA, mutants=True)
+        if dataset == 'closest':
+            if type(colors) != list:
+                samples, data = color.gen_custom_closest(
+                    SAMPLES_DATA, MUTANT_DATA, mutants=True)
+            else:
+                samples, data = color.gen_custom_closest(
+                    SAMPLES_DATA, WT_DATA, colors, mutants=True)
+        if dataset == 'raw':
+            samples, data = SAMPLES_DATA, WT_DATA
 
 
 if __name__ == "__main__":
-    # info, data
-    info, data = gen_custom_closest(['white', 'black'])
-    # Make our phylogenetic Tree
-    #tree = Tree(data)
-    # Color Map
-    color_map = color.fill_cmap(data, on_index=None)
-    # Vizulization Functions
-    #foo_0 = viz_data.show_originaldim
-    #foo_1 = viz_data.PCA_2D
-    #foo_2 = viz_data.PCA_3D
-    foo_3 = viz_data.Load_Features
-
-    vizulizations = [foo_3]
-    for f_viz in vizulizations:
-        # Applied on all data
-        f_viz(data, color_map)
-        # Applied to each family in data
-        viz_data.run_viz_by_field(data, f_viz, color_map, field='scale_color')
-        print('\n')
+    # Intro Text to Our Program
+    txt = pyfiglet.figlet_format('The Scale Project')
+    print(txt)
+    main(colors=['white', 'black'])
