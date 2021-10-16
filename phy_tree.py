f18511fd123eda5ed98abccc3d263eb070ed95e2
Lukas Elsrode <58862171+elsrodeL@users.noreply.github.com>
2021-10-16 14:16:04 -0500

    bugs == features ?

--- phy_tree.py
+++ phy_tree.py
@@ -6,13 +6,12 @@ Class to Visualize the Phylogenetic Relationships/Connections in the Data
                   -- ALL RIGHTS RESERVED 
         
         Lukas Elsrode - Undergraduate Researcher at the Kronforst Laboratory wrote and tested this code 
-        (09/14/2021)
+        (10/16/2021)
 
 """
-# %%
+
 
 import networkx as nx
-import scale_data as data
 import matplotlib.pyplot as plt
 
 # Have some way of vizually differenting diffrent parts of the tree
@@ -20,6 +19,8 @@ DEFAULT_T_NODE_COLOR = 'black'
 DEFAULT_FAM_COLOR = 'red'
 DEFAULT_SPECIES_COLOR = 'green'
 DEFAULT_CONN_COLOR = 'blue'
+
+DEFAULT_GRAPH = nx.Graph()
 
 
 class Trunk:
@@ -34,7 +35,7 @@ class Trunk:
                                         families at the time of divergence. Essentially the nodes that represent a common ancestor not reflected in the data provided.
     """
 
-    def __init__(self):
+    def __init__(self, Graph=DEFAULT_GRAPH):
         """ Trunk Constructor    
             ~ This is all HardCoded in to avoid any mistakes in the phylogenetic relationships between the diffrent families in the DataSet.
         """
@@ -48,15 +49,13 @@ class Trunk:
             'uraniidae',
             'pieridae'
         ]
-        self.graph = self.mk_tree_trunk()
+        self.graph = self.mk_tree_trunk(Graph)
         self.traversal_nodes = [i for i in list(
             self.graph.nodes) if i not in self.all_fams]
 
-    def mk_tree_trunk(self):
+    def mk_tree_trunk(self, G):
         """ Constructs Phylogentic Tree of our ButteryFly and Moths in terms of families.
         """
-        # init a Graph Object
-        G = nx.Graph()
         # mk the root and the diff connectors
         G.add_node('r')
         G.add_node('t0')
@@ -86,8 +85,6 @@ class Trunk:
         return G
 
 
-# Make A Global Stem so it doesn't change if we mess around with it
-global stem
 stem = Trunk()
 
 
@@ -103,6 +100,7 @@ def draw_tree(Tree):
     nx.draw_kamada_kawai(Tree, with_labels=1)
     plt.axis('off')
     plt.show()
+    plt.clf()
     return
 
 
@@ -165,15 +163,18 @@ class Tree:
     def complete(self):
         """ Fills Graph Object out given the input parameters into the class 
         """
-        self.fill_tree()
-        self.simplify_once()
-        draw_tree(self.tree)
-        return
+        self.fill_tree()                    # This fills out the trunk with DataFrame Phylogeny
+        self.simplify_totally()             # Simplify's our Relationships to LCA's only
+        # Add some visual flair to make this easier on the eyes
+        self.add_node_color()
+        draw_tree(self.tree)                # Represent our Phylogony
 
     def fill_tree(self):
         """ Fills The Tree Graph Structure outwardly from the Stem itterating over the 
             DataFrame inputted as a parameter in the constructor 
         """
+        # void inputs
+        voids = ['', 'nan', None]
         # init
         self.tree = self.trunk
         # Get the depth of the Hierarchy you want to Itterate over
@@ -182,33 +183,33 @@ class Tree:
             # Get the corresponding Data to the Current lvl in the tree
             lvl = hierarchy[count]
             nodes = [i for i in set(self.df[lvl].values)]
-            #
+            print('0- ', lvl, nodes)
             for p_node in nodes:
                 node_df = self.df.loc[self.df[lvl] == p_node]
                 sub_lvl = hierarchy[count + 1]
                 children = [i for i in set(node_df[sub_lvl].values)]
 
                 # ADD SUBFAMILY NODE AND CONNECT TO EXISTING TRUNK FAMILY NODE BY EDGE
-                if p_node != '' and count == 0:
+                if p_node not in voids and count == 0:
                     self.tree.add_node(p_node)  # ++ NODE
                     fams = list(
                         set(self.df.loc[self.df[lvl] == p_node]['family'].values))
-                    if len(fams) == 1 and fams[0] != '':
+                    if len(fams) == 1 and fams[0] not in voids:
                         self.tree.add_edge(fams[0], p_node)  # ++ EDGE
 
                 grand_lvl = hierarchy[count - 1]
                 for child in children:
 
                     # ADD NODE TO GRANDPARENT IF PARENT IS VOID
-                    if p_node == '' and child != '':
+                    if p_node in voids and child not in voids:
                         grandparent = list(
                             self.df.loc[self.df[sub_lvl] == child][grand_lvl].values)[0]
-                        if grandparent != '':
+                        if grandparent not in voids:
                             # ++ CHILD <-> GPARENT EDGE
                             self.tree.add_edge(child, grandparent)
                         else:
                             pass
-                    if child == '':
+                    if child in voids:
                         dsub_lvl = hierarchy[count + 2]
                         gchildren = list(
                             self.df.loc[self.df[lvl] == p_node][dsub_lvl].values)
@@ -218,7 +219,7 @@ class Tree:
 
                         # IF THE CHILD IS VOID BUT PARENT IS NOT CONNECT PARENT TO GRANDCHILD
                         for granchild in gr_children:
-                            if granchild != '' and p_node != '':
+                            if granchild not in voids and p_node not in voids:
                                 # ++ G_CHILD <-> PARENT  EDGE
                                 self.tree.add_edge(granchild, p_node)
                             else:
@@ -227,7 +228,7 @@ class Tree:
                     else:
                         # ADD CHILD TO PARENT NODE
                         self.tree.add_node(child)  # ++ CHILD NODE
-                        if p_node != '':
+                        if p_node not in voids:
                             # ++ PARENT <-> CHILD EDGE
                             self.tree.add_edge(p_node, child)
                         else:
@@ -235,68 +236,78 @@ class Tree:
             count += 1
 
         # Remove Manually Constructed Family Nodes which don't appear in our data
-        all_famis = [i for i in set(self.df.family.values) if i != '']
+        all_famis = [i for i in set(
+            self.df.family.values) if i not in voids]
         to_remove = [i for i in self.trunk_fams if i not in all_famis]
         for node in to_remove:
             if node in list(self.tree.nodes):
                 self.tree.remove_node(node)
-        return self.tree
+
+    def remove_nodes(self, nodes):
+        """ Removes the nodes listed from the Phylogentic Tree
+        """
+        for node in nodes:
+            self.tree.remove_node(node)
+        return
+
+    def reduce_nodes(self, nodes):
+        """ Reduces the nodes listed from the Phylogentic Tree
+                i.e : Removes itself and connects the only other two adjacent nodes to each other 
+        """
+        d_adj = self.tree.adj
+        for node in nodes:
+            # get the keys of the value in the adjacency dictionary
+            to_connect = list(d_adj[node].keys())
+            assert len(
+                to_connect) == 2, 'Reducible Node by definition only has two edges'
+            # Remove the node
+            self.remove_nodes([node])
+            # Connect the two nodes that need to be
+            self.tree.add_edge(to_connect[0], to_connect[1])
 
     def simplify_once(self):
+        """ Simplify The given Graph 'G' to remove reducible nodes in the Graph. 
         """
+        # Get nodes and how many connections they each have
+        d_dict, nodes = self.tree.degree, list(self.tree.nodes)
+        # remove reducable Nodes & Leaves of the Tree
+        reducable_nodes, leaves = [i for i in nodes if d_dict[i] == 2 and i not in self.trunk_fams], [
+            i for i in nodes if d_dict[i] == 1]
+        # Reduce Appropriate Nodes
+        self.reduce_nodes(reducable_nodes)
+        # remove any transitionary nodes that may have stuck around after a prune
+        self.remove_nodes(
+            [i for i in leaves if i in self.t_nodes and i != 'r'])
+
+    def simplify_totally(self):
+        """ Simplify the phylogenetic tree to the point it can't be simplified any further 
         """
-        g = self.tree
+        tree, next_tree = 0, 1
+        while tree != next_tree:
+            tree = self.tree
+            self.simplify_once()
+            next_tree = self.tree
+            self.simplify_once()
+        else:
+            return
 
-        d_dict = g.degree
-        nodes = list(g.nodes)
-        adj_dict = dict(g.adj)
-
-        reducables = [i for i in nodes if d_dict[i] == 2]
-        leaves = [i for i in nodes if d_dict[i] == 1]
-        conns = [i for i in nodes if d_dict[i] > 2]
-
-        # initilize a dictionary to connect leaves to their LCA
-        d = {}
-
-        for l in leaves:
-            # Create a Deapth first search list from a leaf
-            search_list = list(nx.dfs_preorder_nodes(g, l))
-
-            # Pointer to list to itterate
-            j = 0
-            cnode = None
-            while cnode not in conns and j < len(search_list):
-                cnode = search_list[j]
-                j += 1
-            else:
-                if l not in d.keys():
-                    d[l] = cnode
-
-        for c in conns:
-            try:
-                # Deapth First Search  itterator
-                search_list = list(nx.dfs_preorder_nodes(g, c))
-                j, x = 0, 0
-                while x not in conns or c == x:
-                    x = search_list[j]
-                    j += 1
-                else:
-                    d[c] = x
-            except:
-                continue
-
-        # They are by def. reducable having only 2 edges
-        for r in reducables:
-            g.remove_node(r)
-
-        for k, v in d.items():
-            g.add_edge(k, v)
-
-        for l in leaves:
-            if l in self.t_nodes:
-                if l != 'r':
-                    g.remove_node(l)
-
-        self.tree = g
-
-        return self.tree
+    def add_node_color(self):
+        """ Changes the Node attributes to be more visually interesting
+        """
+        # Get nodes and how many connections they each have
+        d_dict, nodes = self.tree.degree, list(self.tree.nodes)
+        species = [i for i in nodes if d_dict[i] == 1 and i != 'r']
+        families = [i for i in nodes if i in self.trunk_fams]
+        trans = [i for i in nodes if i in self.t_nodes]
+        # Throwaway to just do a quick group exclusion
+        th_ = []
+        th_.extend(species)
+        th_.extend(families)
+        th_.extend(trans)
+        # get anything that's a subfamily ect
+        conns = [i for i in nodes if i not in set(th_)]
+        set_node_attributes(self.tree, nodes=species,
+                            color=DEFAULT_SPECIES_COLOR)
+        set_node_attributes(self.tree, nodes=families, color=DEFAULT_FAM_COLOR)
+        set_node_attributes(self.tree, nodes=trans, color=DEFAULT_T_NODE_COLOR)
+        set_node_attributes(self.tree, nodes=conns, color=DEFAULT_CONN_COLOR)
